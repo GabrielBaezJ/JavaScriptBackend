4afd270f640b2d3b872b3baf6204a5df7b996501
Báez Jaramillo Gabriel Alejandro <132725537+GabrielBaezJ@users.noreply.github.com>
2026-01-17 00:20:25 -0500

    Update search to use 'college' and add query parameter support

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -2,7 +2,8 @@ const articleModel = require("../models/articleModel");
 
 exports.fetchArticles = async (req, res) => {
     try {
-        const articles = await articleModel.getArticles();
+        const searchTerm = req.query.q || 'college';
+        const articles = await articleModel.getArticles(searchTerm);
         res.json(articles);
     } catch (error) {
         res.status(500).json({ error: "Error fetching articles" });
--- models/articleModel.js
+++ models/articleModel.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-exports.getArticles = async () => {
-    const url = "https://api.plos.org/search?q=title:university";
+exports.getArticles = async (searchTerm = 'college') => {
+    const url = `https://api.plos.org/search?q=title:${searchTerm}`;
 
     const response = await axios.get(url);
     return response.data.response.docs;
